2e479e9db5e6d0a96d57c583d60a554937fea8c1
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-06-23 01:02:17 +0500

    implement basic template

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,69 @@
+"use client";
+
+import {
+  PricingCard,
+  PricingHeader,
+  PricingSwitch,
+} from "@/components/pricing";
+import React, { useState } from "react";
+
 export default function Home() {
-  return <div>Hello Gmind AI</div>;
+  const [isYearly, setIsYearly] = useState(false);
+  const togglePricingPeriod = (value: string) =>
+    setIsYearly(parseInt(value) === 1);
+
+  const plans = [
+    {
+      title: "Basic",
+      monthlyPrice: 10,
+      yearlyPrice: 100,
+      description: "Essential features you need to get started",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+      ],
+      actionLabel: "Get Started",
+    },
+    {
+      title: "Pro",
+      monthlyPrice: 25,
+      yearlyPrice: 250,
+      description: "Perfect for owners of small & medium businessess",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+      ],
+      actionLabel: "Get Started",
+      popular: true,
+    },
+    {
+      title: "Enterprise",
+      price: "Custom",
+      description: "Dedicated support and infrastructure to fit your needs",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+        "Super Exclusive Feature",
+      ],
+      actionLabel: "Contact Sales",
+      exclusive: true,
+    },
+  ];
+  return (
+    <div className="py-8">
+      <PricingHeader
+        title="Pricing Plans"
+        subtitle="Choose the plan that's right for you"
+      />
+      <PricingSwitch onSwitch={togglePricingPeriod} />
+      <section className="flex flex-col sm:flex-row sm:flex-wrap justify-center gap-8 mt-8">
+        {plans.map((plan) => {
+          return <PricingCard key={plan.title} {...plan} isYearly={isYearly} />;
+        })}
+      </section>
+    </div>
+  );
 }
